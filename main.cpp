d0597e7e3513de116c277e38ff3b640e77106d89
Olga <terenteva2002@yandex.ru>
2021-05-05 13:07:46 +0300

    last

--- main.cpp
+++ main.cpp
@@ -1,40 +1,30 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int binarySearch (const int array[], int left, int right, int key) {
+int binary_Search (vector<int> arr, int key) {
+    int middle = 0;
+    int left = 0;
+    int right;
+    right = sizeof(arr) - 1;
 
-    int middle;
     while (true) {
         middle = (left + right) / 2;
-        if (key < array[middle])
-            right = middle -1 ;
-        else if (key > array[middle])
-            left = middle + 1;
-        else return middle;
 
-        if (left > right) return -1;
+        if (key < arr[middle])
+            right = middle - 1;
+        else if (key > arr[middle])
+            left = middle + 1;
+        else
+            return middle;
+
+        if (left > right)
+            return -1;
     }
 }
 
-
 int main() {
-  const int SIZE = 12;
-  int array[SIZE] = {};
-  int key = 0;
-  int index;
-
-  for (int i = 0; i < SIZE; i++) {
-      array[i] = i + 1;
-      cout << array[i];
-  }
-  cin >> key;
-
-  index = binarySearch(array, 0, SIZE, key);
-
-  if (index >= 0)
-      cout << "Указанное число находится в ячейке с индексом: " << index << " ";
-  else
-      cout << "Такого числа в массиве нет!";
-
-    return 0;
+    vector<int> arr = {1, 2, 3, 4, 5, 10, 12};
+    cout << binary_Search(arr, 10);
 }
